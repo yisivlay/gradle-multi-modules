ce4153d3752aba40a192c4a4436ce16de20b7b11
YISivlay <sivlayyi.bs@gmail.com>
2024-09-13 23:57:19 +0700

    create demo gradle run multiple modules.

---
+++ app/src/main/java/com/base/Main.java
@@ -0,0 +1,26 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "com.base")
+@RestController
+public class Main {
+
+    private final MyService myService;
+
+    public Main(MyService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return myService.message();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
